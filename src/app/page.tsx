42462ddceb95dd645b1e1d6a371685b2ab93f6d4
Seokho <seokho0120@gmail.com>
2023-09-25 16:18:21 +0900

    ⚡️ 팔로잉바 - 스타일링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,11 +15,11 @@ export default async function HomePage() {
 
   return (
     <section className='w-full flex flex-col md:flex-row max-w-[850px] p-4'>
-      <div className='w-full basis-3/4'>
+      <div className='w-full basis-3/4 min-w-0'>
         <FollowingBar />
         <PostList />
       </div>
-      <div className='basis-1/4'>
+      <div className='basis-1/4 ml-8'>
         <SideBar user={user} />
       </div>
     </section>
--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -7,10 +7,16 @@ import Avatar from './Avatar';
 
 export default function FollowingBar() {
   const { data, isLoading: loading, error } = useSWR<DetailUser>('/api/me');
-  const users = data?.following;
+  // const users = data?.following;
+  // const users = undefined;
+  const users = data?.following && [
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+  ];
 
   return (
-    <section>
+    <section className='w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto'>
       {loading ? (
         <PropagateLoader size={8} color='red' />
       ) : (
@@ -18,12 +24,17 @@ export default function FollowingBar() {
       )}
 
       {users && users.length > 0 && (
-        <ul>
+        <ul className='w-full flex gap-2'>
           {users.map(({ username, image }) => (
             <li key={username}>
-              <Link href={`/users/${username}`}>
+              <Link
+                className='flex flex-col items-center w-20'
+                href={`/users/${username}`}
+              >
                 <Avatar image={image} highlight />
-                <p>{username}</p>
+                <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>
+                  {username}
+                </p>
               </Link>
             </li>
           ))}
