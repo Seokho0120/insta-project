504be69da0bd22a8ecd98d48927cd9efe4d0dca2
Seokho <seokho0120@gmail.com>
2023-09-26 18:37:47 +0900

    ⚡️ SEO최적화, build후 force-dynamic 처리

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,8 +1,14 @@
 import Signin from '@/components/Signin';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { Metadata } from 'next';
 import { getServerSession } from 'next-auth';
 import { getProviders } from 'next-auth/react';
 import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: 'Signin',
+  description: 'Signup or Login to Instantgram',
+};
 
 type Props = {
   searchParams: {
--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,15 +1,19 @@
 import UserPosts from '@/components/UserPosts';
 import UserProfile from '@/components/UserProfile';
 import { getUserForProfile } from '@/service/user';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
-import React from 'react';
+import React, { cache } from 'react';
 
-type Props = {
-  params: { username: string };
-};
+type Props = { params: { username: string } };
+
+const getUser = cache(async (username: string) => getUserForProfile(username));
+// metadata에서도 getUserForProfile함수 호출, UserPage에서도 getUserForProfile함수 호출함
+// 한번 렌더링되는 사이클내에서 여러번 사용하지 않고 재사용하려면? cache를 쓰면됨
+// 전달하는 username이 변하지 않으면, 동일한 사용자한에서 캐싱된 username을 사용함
 
 export default async function UserPage({ params: { username } }: Props) {
-  const user = await getUserForProfile(username);
+  const user = await getUser(username);
 
   if (!user) {
     notFound();
@@ -21,4 +25,15 @@ export default async function UserPage({ params: { username } }: Props) {
       <UserPosts user={user} />
     </section>
   );
+}
+
+export async function generateMetadata({
+  params: { username },
+}: Props): Promise<Metadata> {
+  const user = await getUser(username);
+
+  return {
+    title: `${user?.name} (@${user?.username}) · Instantgram Photos`,
+    description: `${user?.name}'s all Instantgram posts`,
+  };
 }
