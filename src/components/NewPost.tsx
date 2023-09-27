dcd526061fdbba54649be658cc86e6089fbc373d
Seokho <seokho0120@gmail.com>
2023-09-27 23:12:38 +0900

    ⚡️ 새포스트 작성 - 클라이언트

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,10 +1,12 @@
 'use client';
-import { AuthUser } from '@/model/user';
+import { ChangeEvent, FormEvent, useRef, useState } from 'react';
 import Image from 'next/image';
-import { ChangeEvent, useState } from 'react';
+import { AuthUser } from '@/model/user';
 import PostUserAvatar from './PostUserAvatar';
 import Button from './ui/Button';
 import FilesIcon from './ui/icons/FilesIcon';
+import { useRouter } from 'next/navigation';
+import GridSpinner from './ui/GridSpinner';
 
 type Props = {
   user: AuthUser;
@@ -13,6 +15,10 @@ type Props = {
 export default function NewPost({ user: { username, image } }: Props) {
   const [dragging, setDragging] = useState(false);
   const [file, setFile] = useState<File>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>();
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const router = useRouter();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -43,14 +49,44 @@ export default function NewPost({ user: { username, image } }: Props) {
     const files = e.dataTransfer?.files;
     if (files && files[0]) {
       setFile(files[0]);
-      console.log('files[0]', files[0]);
     }
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!file) return;
+    setLoading(true);
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('text', textRef.current?.value ?? '');
+
+    fetch('/api/posts/', { method: 'POST', body: formData }) //
+      .then((res) => {
+        if (!res.ok) {
+          setError(`${res.status} ${res.statusText}`);
+          return;
+        }
+        router.push('/');
+      })
+      .catch((error) => setError(error.toString()))
+      .finally(() => setLoading(false));
   };
 
   return (
     <section className='w-full max-w-xl flex flex-col items-center mt-6'>
+      {loading && (
+        <div className='absolute inset-0 z-20 text-center pt-[30%] bg-sky-500/20'>
+          <GridSpinner />
+        </div>
+      )}
+      {error && (
+        <p className='w-full bg-red-100 text-red-600 text-center p-4 mb-4 font-bold'>
+          {error}
+        </p>
+      )}
       <PostUserAvatar username={username} image={image ?? ''} />
-      <form className='w-full flex flex-col mt-2'>
+      <form className='w-full flex flex-col mt-2' onSubmit={handleSubmit}>
         <input
           className='hidden'
           name='input'
@@ -97,6 +133,7 @@ export default function NewPost({ user: { username, image } }: Props) {
           required
           rows={10}
           placeholder={'Write a caption...'}
+          ref={textRef}
         />
         <Button text='Publish' onClick={() => {}} />
       </form>
