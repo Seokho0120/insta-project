bcec82d82cb7acf005561493b0eb9e7ce0017e17
Seokho <seokho0120@gmail.com>
2023-09-27 16:47:02 +0900

    🐛 Avatar 긴 댓글 프로필 찌그러짐 수정

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -25,7 +25,7 @@ export default function Avatar({
 }
 
 function getContainerStyle(size: AvatarSize, highlight: boolean): string {
-  const baseStyle = 'rounded-full flex justify-center items-center';
+  const baseStyle = 'rounded-full flex justify-center items-center shrink-0';
   const highlightStyle = highlight
     ? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300'
     : '';
