9c7bd834cb51036cd69160dad92eac28d25e93f5
Seokho <seokho0120@gmail.com>
2023-09-27 17:01:23 +0900

    ⚡️ 팔로우 버튼 - 서버

--- src/service/user.ts
+++ src/service/user.ts
@@ -97,3 +97,28 @@ export async function removeBookmark(userId: string, postId: string) {
     .unset([`bookmarks[_ref=="${postId}"]`])
     .commit();
 }
+
+export async function follow(myId: string, targetId: string) {
+  return client
+    .transaction() //
+    .patch(myId, (user) =>
+      user
+        .setIfMissing({ following: [] })
+        .append('following', [{ _ref: targetId, _type: 'reference' }])
+    )
+    .patch(myId, (user) =>
+      user
+        .setIfMissing({ followers: [] })
+        .append('followers', [{ _ref: myId, _type: 'reference' }])
+    )
+    .commit({ autoGenerateArrayKeys: true });
+}
+
+export async function unFollow(myId: string, targetId: string) {
+  return client
+    .transaction() //
+    .patch(myId, (user) => user.unset([`following[_ref == "${targetId}"]`]))
+    .patch(targetId, (user) => user.unset([`followers[_ref == "${myId}"]`]))
+
+    .commit({ autoGenerateArrayKeys: true });
+}
