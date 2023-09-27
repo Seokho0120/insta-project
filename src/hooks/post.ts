e77d2289d48b7d0939fdc772cfe0e01168198b48
Seokho <seokho0120@gmail.com>
2023-09-27 16:42:57 +0900

    ⚡️ 코멘트 - 성능개선 리팩토링

--- src/hooks/post.ts
+++ src/hooks/post.ts
@@ -1,5 +1,6 @@
 import { Comment, FullPost } from '@/model/post';
-import useSWR from 'swr';
+import { useCallback } from 'react';
+import useSWR, { useSWRConfig } from 'swr';
 
 async function addComment(id: string, comment: string) {
   return fetch('/api/comments', {
@@ -16,19 +17,25 @@ export default function useFullPost(postId: string) {
     mutate,
   } = useSWR<FullPost>(`/api/posts/${postId}`);
 
-  const postComment = (comment: Comment) => {
-    if (!post) return;
-    const newPost = {
-      ...post,
-      comments: [...post.comments, comment],
-    };
+  const { mutate: globalMutate } = useSWRConfig();
 
-    return mutate(addComment(post.id, comment.comment), {
-      optimisticData: newPost,
-      populateCache: false,
-      revalidate: false,
-      rollbackOnError: true,
-    });
-  };
+  const postComment = useCallback(
+    (comment: Comment) => {
+      if (!post) return;
+      const newPost = {
+        ...post,
+        comments: [...post.comments, comment],
+      };
+
+      return mutate(addComment(post.id, comment.comment), {
+        optimisticData: newPost,
+        populateCache: false,
+        revalidate: false,
+        rollbackOnError: true,
+      }).then(() => globalMutate('/api/posts'));
+    },
+    [post, mutate, globalMutate]
+  );
+
   return { post, isLoading, error, postComment };
 }
--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -1,4 +1,5 @@
 import { Comment, SimplePost } from '@/model/post';
+import { useCallback } from 'react';
 import useSWR from 'swr';
 
 async function updateLike(id: string, like: boolean) {
@@ -23,36 +24,43 @@ export default function usePosts() {
     mutate,
   } = useSWR<SimplePost[]>('/api/posts');
 
-  const setLike = (post: SimplePost, username: string, like: boolean) => {
-    const newPost = {
-      ...post,
-      likes: like
-        ? [...post.likes, username]
-        : post.likes.filter((item) => item !== username),
-    };
-    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+  const setLike = useCallback(
+    (post: SimplePost, username: string, like: boolean) => {
+      const newPost = {
+        ...post,
+        likes: like
+          ? [...post.likes, username]
+          : post.likes.filter((item) => item !== username),
+      };
+      const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
 
-    return mutate(updateLike(post.id, like), {
-      optimisticData: newPosts,
-      populateCache: false,
-      revalidate: false,
-      rollbackOnError: true,
-    });
-  };
+      return mutate(updateLike(post.id, like), {
+        optimisticData: newPosts,
+        populateCache: false,
+        revalidate: false,
+        rollbackOnError: true,
+      });
+    },
+    [posts, mutate]
+  );
 
-  const postComment = (post: SimplePost, comment: Comment) => {
-    const newPost = {
-      ...post,
-      comments: post.comments + 1,
-    };
-    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+  const postComment = useCallback(
+    (post: SimplePost, comment: Comment) => {
+      const newPost = {
+        ...post,
+        comments: post.comments + 1,
+      };
+      const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
 
-    return mutate(addComment(post.id, comment.comment), {
-      optimisticData: newPosts,
-      populateCache: false,
-      revalidate: false,
-      rollbackOnError: true,
-    });
-  };
+      return mutate(addComment(post.id, comment.comment), {
+        optimisticData: newPosts,
+        populateCache: false,
+        revalidate: false,
+        rollbackOnError: true,
+      });
+    },
+    [posts, mutate]
+  );
+
   return { posts, isLoading, error, setLike, postComment };
 }
